8bc4f25e4e2ca426925da2686d0152592671faec
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-27 14:57:50 -0400

    todo comment

--- src/main/java/com/logstash/FieldReference.java
+++ src/main/java/com/logstash/FieldReference.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// TODO: implement thread-safe path cache singleton to avoid parsing
+
 public class FieldReference {
 
     private List<String> path;
