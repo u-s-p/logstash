adec6b383c88dbbb06201fbd8fda482b93867c41
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-28 23:19:13 -0400

    wrap all Event methods

--- src/main/java/com/logstash/ext/JrubyEventExtLibrary.java
+++ src/main/java/com/logstash/ext/JrubyEventExtLibrary.java
@@ -2,6 +2,7 @@ package com.logstash.ext;
 
 import com.logstash.Event;
 import com.logstash.EventImpl;
+import com.logstash.Timestamp;
 import org.jruby.*;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
@@ -35,9 +36,17 @@ public class JrubyEventExtLibrary implements Library {
             super(runtime, klass);
         }
 
+        public Event getEvent() {
+            return event;
+        }
+
+        public void setEvent(Event event) {
+            this.event = event;
+        }
+
         // def initialize(data = {})
         @JRubyMethod(name = "initialize", optional = 1)
-        public IRubyObject initialize(ThreadContext context, IRubyObject[] args)
+        public IRubyObject ruby_initialize(ThreadContext context, IRubyObject[] args)
         {
             args = Arity.scanArgs(context.runtime, args, 0, 1);
             IRubyObject data = args[0];
@@ -52,33 +61,124 @@ public class JrubyEventExtLibrary implements Library {
             return context.nil;
         }
 
-        // def to_java
-        @JRubyMethod(name = "to_java")
-        public IRubyObject toJava(ThreadContext context)
-        {
-            return JavaUtil.convertJavaToUsableRubyObject(context.runtime, this.event);
-        }
-
-        // def to_java
-        @JRubyMethod(name = "to_json")
-        public IRubyObject toJson(ThreadContext context)
-            throws IOException
-        {
-            return RubyString.newString(context.runtime, event.toJson());
-        }
-
         @JRubyMethod(name = "[]", required = 1)
-        public IRubyObject getField(ThreadContext context, RubyString reference)
+        public IRubyObject ruby_get_field(ThreadContext context, RubyString reference)
         {
             return JavaUtil.convertJavaToRuby(context.runtime, this.event.getField(reference.asJavaString()));
         }
 
         @JRubyMethod(name = "[]=", required = 2)
-        public IRubyObject setField(ThreadContext context, RubyString reference, IRubyObject value)
+        public IRubyObject ruby_set_field(ThreadContext context, RubyString reference, IRubyObject value)
         {
             // TODO: add value type guard here?
             this.event.setField(reference.asJavaString(), value);
             return value;
+        }
+
+        @JRubyMethod(name = "cancel")
+        public IRubyObject ruby_cancel(ThreadContext context)
+        {
+            this.event.cancel();
+            return RubyBoolean.createTrueClass(context.runtime);
+        }
+
+        @JRubyMethod(name = "uncancel")
+        public IRubyObject ruby_uncancel(ThreadContext context)
+        {
+            this.event.uncancel();
+            return RubyBoolean.createFalseClass(context.runtime);
+        }
+
+        @JRubyMethod(name = "cancelled?")
+        public IRubyObject ruby_cancelled(ThreadContext context)
+        {
+            return RubyBoolean.newBoolean(context.runtime, this.event.isCancelled());
+        }
+
+        @JRubyMethod(name = "timestamp")
+        public IRubyObject ruby_get_timestamp(ThreadContext context)
+        {
+            // TODO: properly implement
+            return JavaUtil.convertJavaToRuby(context.runtime, this.event.getTimestamp());
+        }
+
+        @JRubyMethod(name = "timestamp=", required = 1)
+        public IRubyObject ruby_set_timestamp(ThreadContext context, IRubyObject value)
+        {
+            // TODO: properly implement
+            this.event.setTimestamp((Timestamp)value.toJava(Timestamp.class));
+            return JavaUtil.convertJavaToRuby(context.runtime, this.event.getTimestamp());
+        }
+
+        @JRubyMethod(name = "include?", required = 1)
+        public IRubyObject ruby_includes(ThreadContext context, RubyString reference)
+        {
+            return RubyBoolean.newBoolean(context.runtime, this.event.includes(reference.asJavaString()));
+        }
+
+        @JRubyMethod(name = "remove", required = 1)
+        public IRubyObject ruby_remove(ThreadContext context, RubyString reference)
+        {
+            return JavaUtil.convertJavaToRuby(context.runtime, this.event.remove(reference.asJavaString()));
+        }
+
+        @JRubyMethod(name = "clone")
+        public IRubyObject ruby_clone(ThreadContext context)
+        {
+            // TODO: no idea about getClassClass - wild guess - need to understasn how to properly instantiate a RubyEvent
+            RubyEvent result = new RubyEvent(context.runtime, context.runtime.getClassClass());
+            result.setEvent(this.event.clone());
+            return result;
+        }
+
+        @JRubyMethod(name = "overwrite", required = 1)
+        public IRubyObject ruby_overwrite(ThreadContext context, IRubyObject value)
+        {
+            // TODO: no idea about getClassClass - wild guess - need to understasn how to properly instantiate a RubyEvent
+            RubyEvent result = new RubyEvent(context.runtime, context.runtime.getClassClass());
+            result.setEvent(this.event.overwrite(((RubyEvent)value).event));
+            return result;
+        }
+
+        @JRubyMethod(name = "append", required = 1)
+        public IRubyObject ruby_append(ThreadContext context, IRubyObject value)
+        {
+            // TODO: no idea about getClassClass - wild guess - need to understasn how to properly instantiate a RubyEvent
+            RubyEvent result = new RubyEvent(context.runtime, context.runtime.getClassClass());
+            result.setEvent(this.event.append(((RubyEvent)value).event));
+            return result;
+        }
+
+        @JRubyMethod(name = "sprintf", required = 1)
+        public IRubyObject ruby_sprintf(ThreadContext context, RubyString format)
+        {
+            return RubyString.newString(context.runtime, event.sprintf(format.asJavaString()));
+        }
+
+        @JRubyMethod(name = "to_s")
+        public IRubyObject ruby_to_s(ThreadContext context)
+        {
+            return RubyString.newString(context.runtime, event.toString());
+        }
+
+        @JRubyMethod(name = "to_hash")
+        public IRubyObject ruby_to_hash(ThreadContext context)
+        {
+            // TODO: should we explicitely convert to RubyHash?
+            return JavaUtil.convertJavaToUsableRubyObject(context.runtime, this.event.toMap());
+        }
+
+        @JRubyMethod(name = "to_java")
+        public IRubyObject ruby_to_java(ThreadContext context)
+        {
+            return JavaUtil.convertJavaToUsableRubyObject(context.runtime, this.event);
+        }
+
+        @JRubyMethod(name = "to_json")
+        public IRubyObject ruby_to_json(ThreadContext context)
+            throws IOException
+        {
+            return RubyString.newString(context.runtime, event.toJson());
         }
     }
 }
