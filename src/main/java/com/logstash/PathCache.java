4689c0d6840111ee0603225144299ff8e79ef25e
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-29 18:01:20 -0400

    pre cache timestamp and expose isTimestamp

--- src/main/java/com/logstash/PathCache.java
+++ src/main/java/com/logstash/PathCache.java
@@ -7,7 +7,17 @@ public class PathCache {
     private static PathCache instance = null;
     private static ConcurrentHashMap<String, FieldReference> cache = new ConcurrentHashMap<>();
 
-    protected PathCache() {}
+    private FieldReference timestamp;
+
+    // TODO: dry with Event
+    public static final String TIMESTAMP = "@timestamp";
+    public static final String BRACKETS_TIMESTAMP = "[" + TIMESTAMP + "]";
+
+    protected PathCache() {
+        // inject @timestamp
+        this.timestamp = cache(TIMESTAMP);
+        cache(BRACKETS_TIMESTAMP, this.timestamp);
+    }
 
     public static PathCache getInstance() {
         if (instance == null) {
@@ -16,7 +26,11 @@ public class PathCache {
         return instance;
     }
 
-    FieldReference cache(String reference) {
+    public boolean isTimestamp(String reference) {
+        return (cache(reference) == this.timestamp);
+    }
+
+    public FieldReference cache(String reference) {
         // atomicity between the get and put is not important
         FieldReference result = cache.get(reference);
         if (result == null) {
@@ -24,5 +38,10 @@ public class PathCache {
             cache.put(reference, result);
         }
         return result;
+    }
+
+    public FieldReference cache(String reference, FieldReference field) {
+        cache.put(reference, field);
+        return field;
     }
 }
