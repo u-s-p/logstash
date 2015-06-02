4fb710d78043ace83c2ec789876d694994cb00a9
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-06-02 14:58:07 -0400

    Make the #sprintf method work with Array and Hash

--- src/main/java/com/logstash/EpochNode.java
+++ src/main/java/com/logstash/EpochNode.java
@@ -1,5 +1,7 @@
 package com.logstash;
 
+import java.io.IOException;
+
 /**
  * Created by ph on 15-05-22.
  */
@@ -7,7 +9,7 @@ public class EpochNode implements TemplateNode {
     public EpochNode(){ }
 
     @Override
-    public String evaluate(Event event) {
+    public String evaluate(Event event) throws IOException {
         return String.valueOf(event.getTimestamp().getTime().getMillis() / 1000);
     }
 }
--- src/test/java/com/logstash/StringInterpolationTest.java
+++ src/test/java/com/logstash/StringInterpolationTest.java
@@ -4,6 +4,8 @@ package com.logstash;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +14,7 @@ import static org.junit.Assert.*;
 
 public class StringInterpolationTest {
     @Test
-    public void testCompletelyStaticTemplate() {
+    public void testCompletelyStaticTemplate() throws IOException {
         Event event = getTestEvent();
         String path = "/full/path/awesome";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -21,7 +23,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void testOneLevelField() {
+    public void testOneLevelField() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{bar}/awesome";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -30,7 +32,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void testMultipleLevelField() {
+    public void testMultipleLevelField() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{bar}/%{awesome}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -39,7 +41,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void testMissingKey() {
+    public void testMissingKey() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{do-not-exist}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -48,7 +50,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void testDateFormater() {
+    public void testDateFormater() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{+YYYY}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -57,7 +59,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void TestMixDateAndFields() {
+    public void TestMixDateAndFields() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{+YYYY}/weeee/%{bar}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -66,7 +68,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void testUnclosedTag() {
+    public void testUnclosedTag() throws IOException {
         Event event = getTestEvent();
         String path = "/full/%{+YYY/web";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -75,7 +77,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void TestStringIsOneDateTag() {
+    public void TestStringIsOneDateTag() throws IOException {
         Event event = getTestEvent();
         String path = "%{+YYYY}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -83,7 +85,7 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void TestFieldRef() {
+    public void TestFieldRef() throws IOException {
         Event event = getTestEvent();
         String path = "%{[j][k1]}";
         StringInterpolation si = StringInterpolation.getInstance();
@@ -91,11 +93,34 @@ public class StringInterpolationTest {
     }
 
     @Test
-    public void TestEpoch() {
+    public void TestEpoch() throws IOException {
         Event event = getTestEvent();
         String path = "%{+%s}";
         StringInterpolation si = StringInterpolation.getInstance();
         assertEquals("1443672000", si.evaluate(event, path));
+    }
+
+    @Test
+    public void TestValueIsArray() throws IOException {
+        ArrayList l = new ArrayList();
+        l.add("Hello");
+        l.add("world");
+
+        Event event = getTestEvent();
+        event.setField("message", l);
+
+        String path = "%{message}";
+        StringInterpolation si = StringInterpolation.getInstance();
+        assertEquals("Hello,world", si.evaluate(event, path));
+    }
+
+    @Test
+    public void TestValueIsHash() throws IOException {
+        Event event = getTestEvent();
+
+        String path = "%{j}";
+        StringInterpolation si = StringInterpolation.getInstance();
+        assertEquals("{\"k1\":\"v\"}", si.evaluate(event, path));
     }
 
     public Event getTestEvent() {
